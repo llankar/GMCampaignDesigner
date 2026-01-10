db7267936a9918f8a0619d790ffaa03e05aceefb
llankar <121680998+llankar@users.noreply.github.com>
2026-01-10 17:40:34 +0100

    Add text import dialog for UI

---
+++ modules/ui/imports/__init__.py
@@ -0,0 +1,19 @@
+from modules.ui.imports.text_import_dialog import TextImportDialog
+from modules.ui.imports.text_import_mappings import (
+    TextImportTarget,
+    TARGETS,
+    build_source_metadata,
+    extract_default_name,
+    list_target_labels,
+    target_for_label,
+)
+
+__all__ = [
+    "TextImportDialog",
+    "TextImportTarget",
+    "TARGETS",
+    "build_source_metadata",
+    "extract_default_name",
+    "list_target_labels",
+    "target_for_label",
+]
