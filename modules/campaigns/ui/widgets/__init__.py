61dabe036291e41d3090f12906d903b016d4cef7
llankar <121680998+llankar@users.noreply.github.com>
2026-03-20 21:10:14 +0100

    Polish campaign arc editor UI

--- modules/campaigns/ui/widgets/__init__.py
+++ modules/campaigns/ui/widgets/__init__.py
@@ -1,4 +1,5 @@
 from .date_field import CampaignDateField
+from .form_section import FormSection
 from .scenario_multi_selector import ScenarioMultiSelector
 
-__all__ = ["CampaignDateField", "ScenarioMultiSelector"]
+__all__ = ["CampaignDateField", "FormSection", "ScenarioMultiSelector"]
