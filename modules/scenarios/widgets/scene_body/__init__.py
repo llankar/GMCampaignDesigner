d4c94ec8a156944826b6ccf5c5e591b58a524c9e
llankar <121680998+llankar@users.noreply.github.com>
2026-03-27 22:11:17 +0100

    Improve scene entity groups sorting, headers, and badges

--- modules/scenarios/widgets/scene_body/__init__.py
+++ modules/scenarios/widgets/scene_body/__init__.py
@@ -1,3 +1,4 @@
 from .entities_grid import create_entities_groups_grid
+from .entities_sorting import prepare_entities_for_group
 
-__all__ = ["create_entities_groups_grid"]
+__all__ = ["create_entities_groups_grid", "prepare_entities_for_group"]
